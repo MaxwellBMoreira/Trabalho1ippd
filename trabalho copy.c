5075440eaab4addbfeadfb93d5e64becf8483dd0
Maxwell Moreira <maxwellburkemoreira@gmail.com>
2022-10-21 14:54:18 -0300

    leitura quadro referencia e atual

--- trabalho copy.c
+++ trabalho copy.c
@@ -16,13 +16,16 @@ int readYUV(const char *filename, uint32_t width, uint32_t height)
 		return 1;
 	}
 	int	frameSize=width*height;
-    unsigned char YUV[width][height];
-	//*YUV = malloc((width*height)*sizeof(int));
+    unsigned char Y[width][height];
+    unsigned char Y2[width][height];
+    unsigned char UV[width/2][height/2];
+	//*Y = malloc((width*height)*sizeof(int));
 
-	fread(YUV, sizeof(unsigned char[width][height]), 1, fp);
+	fread(Y, sizeof(unsigned char[width][height]), 1, fp);// LE PRIMEIRO FRAME
     /*for(int i=0;i<frameSize;i++){
-        YUV[i]=i+1;
+        Y[i]=i+1;
     }*/
+
     printf("!!!!\n");
 
 	/*if (frame != frameSize) {
@@ -33,18 +36,36 @@ int readYUV(const char *filename, uint32_t width, uint32_t height)
 
 
     printf("Filename: %s \nWidth: %d \nHeight: %d \nFrameSize: %d\n",filename,width,height,frameSize);
+    int teste;
+    scanf("%d",&teste);
     int i,j;
 
     for(i=0;i<width;i++){
         printf("\n");
         for(j=0;j<height;j++){
-            //printf("[%d/%d=%d]  ",i,j,YUV[i*height+j]);
-            printf("%d  ",YUV[i][j]);
+            //printf("[%d/%d=%d]  ",i,j,Y[i*height+j]);
+            printf("%d  ",Y[i][j]);
         }
-        
     }
 
+    printf("Frame 1\n");
 
+    
+    //fread(UV, sizeof(unsigned char[width/2][height/2]), 1, fp); //LER TAMANHO UV PARA SER DESCARTADO
+    printf("Lido UV 1\n");
+    scanf("%d",&teste);
+    fread(Y2, sizeof(unsigned char[width][height]), 1, fp); //LE SEGUNDO FRAME
+    printf("Lido Frame 2\n");
+    scanf("%d",&teste);
+    
+    for(i=0;i<width;i++){
+        printf("\n");
+        for(j=0;j<height;j++){
+            //printf("[%d/%d=%d]  ",i,j,Y[i*height+j]);
+            printf("%d  ",Y2[i][j]);
+        }
+ 
+    }
 
 	fclose(fp);
 	return 0;
@@ -62,7 +83,7 @@ int main(int argc, char *argv[]){
     const char *filename = argv[1];
     uint32_t width, height;
     const char *out;
-    uint8_t *YUV;
+    uint8_t *Y;
 
     width = atoi(argv[2]);
     height = atoi(argv[3]);
--- trabalho.c
+++ trabalho.c
@@ -8,46 +8,52 @@
 
 int readYUV(const char *filename, uint32_t width, uint32_t height)
 {
-	FILE *fp;
-    fp = fopen(filename, "rb");
-	if(!fp)
-	{
-		perror("Error opening yuv image for read");
-		return 1;
-	}
-	int	frameSize=width*height;
-    unsigned char YUV[width][height];
-	//*YUV = malloc((width*height)*sizeof(int));
+	/*
+	//*Y = malloc((width*height)*sizeof(int));
 
-	fread(YUV, sizeof(unsigned char[width][height]), 1, fp);
+	
     /*for(int i=0;i<frameSize;i++){
-        YUV[i]=i+1;
+        Y[i]=i+1;
     }*/
+
     printf("!!!!\n");
 
 	/*if (frame != frameSize) {
 		perror("Error reading yuv image");
 		fclose(fp);
 		return 3;
-	}*/
+	}
 
 
     printf("Filename: %s \nWidth: %d \nHeight: %d \nFrameSize: %d\n",filename,width,height,frameSize);
-    int i,j;
+    int teste;
+    scanf("%d",&teste);
+    
 
+    
+
+    printf("Frame 1\n");
+
+    
+    //fread(UV, sizeof(unsigned char[width/2][height/2]), 1, fp); //LER TAMANHO UV PARA SER DESCARTADO
+    printf("Lido UV 1\n");
+    scanf("%d",&teste);
+    fread(Y2, sizeof(unsigned char[width][height]), 1, fp); //LE SEGUNDO FRAME
+    printf("Lido Frame 2\n");
+    scanf("%d",&teste);
+    
     for(i=0;i<width;i++){
         printf("\n");
         for(j=0;j<height;j++){
-            //printf("[%d/%d=%d]  ",i,j,YUV[i*height+j]);
-            printf("%d  ",YUV[i][j]);
+            //printf("[%d/%d=%d]  ",i,j,Y[i*height+j]);
+            printf("%d  ",Y2[i][j]);
         }
-        
+ 
     }
 
-
-
-	fclose(fp);
+	
 	return 0;
+*/
 }
 
 
@@ -61,17 +67,96 @@ int main(int argc, char *argv[]){
     }
     const char *filename = argv[1];
     uint32_t width, height;
-    const char *out;
-    uint8_t *YUV;
 
     width = atoi(argv[2]);
     height = atoi(argv[3]);
-    
-    // read input data and allocate output data
-    if(readYUV(filename, width, height)!=0)
-    {
-        printf("Error reading image file, check that the file exists and has the correct format and resolution.\n");
-        return 1;
+
+    //DEFINIÇÃO DO ARQUIVO DE LEITURA E TAMANHO DOS FRAMES
+    FILE *fp;
+    fp = fopen(filename, "rb");
+	if(!fp)
+	{
+		perror("Error opening yuv image for read");
+		return 1;
+	}
+
+	int	frameSize=width*height;
+    unsigned char reference[width][height];
+    unsigned char atual[width][height];
+    unsigned char U[width/4][height/4];
+    unsigned char V[width/4][height/4];
+    unsigned char blocoComp[8][8];
+    int blockCounter=1;
+    int i,j,teste;
+
+    //leitura frame referencia
+    printf("Frame Referencia\n");
+    scanf("%d",&teste);
+    fread(reference, sizeof(unsigned char[width][height]), 1, fp);// LE PRIMEIRO FRAME
+    for(i=0;i<width;i++){
+        if(i<8)
+            printf("\n");
+        for(j=0;j<height;j++){
+            //printf("[%d/%d=%d]  ",i,j,Y[i*height+j]);
+            if(i<8 & j<8)
+                printf("%d  ",reference[i][j]);
+        }
     }
 
+    //printf("Canal U\n");
+    //scanf("%d",&teste);
+    //leitura dos descartes
+    fread(U, sizeof(unsigned char[width/4][height/4]), 1, fp);// LE PRIMEIRO FRAME
+    /*for(i=0;i<width/4;i++){
+        printf("\n");
+        for(j=0;j<height/4;j++){
+            //printf("[%d/%d=%d]  ",i,j,Y[i*height+j]);
+            printf("%d  ",U[i][j]);
+        }
+    }*/
+
+
+    //printf("Canal V\n");
+    //("%d",&teste);
+    fread(V, sizeof(unsigned char[width/4][height/4]), 1, fp);// LE PRIMEIRO FRAME
+    /*for(i=0;i<width/4;i++){
+        printf("\n");
+        for(j=0;j<height/4;j++){
+            //printf("[%d/%d=%d]  ",i,j,Y[i*height+j]);
+            printf("%d  ",V[i][j]);
+        }
+    }*/
+
+    printf("Frame atual\n");
+    scanf("%d",&teste);
+    fread(atual, sizeof(unsigned char[width][height]), 1, fp);// LE PRIMEIRO FRAME
+    for(i=0;i<width;i++){
+        printf("\n");
+        for(j=0;j<height;j++){
+            //printf("[%d/%d=%d]  ",i,j,Y[i*height+j]);
+            printf("%d  ",atual[i][j]);
+        }
+    }
+
+
+
+    //comparação entre referencia e atual
+    scanf("%d",&teste);
+    for(i=0;i<8;i++){
+        for(j=0;j<8;j++){
+            //printf("[%d/%d=%d]  ",i,j,Y[i*height+j]);
+            blocoComp[i][j]=reference[i][j];
+        }
+    }
+
+    for(i=0;i<8;i++){
+        printf("\n");
+        for(j=0;j<8;j++){
+            //printf("[%d/%d=%d]  ",i,j,Y[i*height+j]);
+            printf("%d ",blocoComp[i][j]);
+        }
+    }
+
+    fclose(fp);
+    return 1;
  }
